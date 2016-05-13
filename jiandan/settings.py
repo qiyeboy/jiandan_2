63ec5df7fdfe33bbdbd5616ef7a41f03dab55643
waqxr <594675898@qq.com>
2016-05-13 16:16:59 +0800

    增加突破反爬虫机制

--- jiandan/settings.py
+++ jiandan/settings.py
@@ -28,7 +28,7 @@ IMAGES_THUMBS = {#缩略图的尺寸，设置这个值就会产生缩略图
 }
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'jiandan (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:46.0) Gecko/20100101 Firefox/46.0'
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS=32
@@ -48,10 +48,12 @@ IMAGES_THUMBS = {#缩略图的尺寸，设置这个值就会产生缩略图
 #TELNETCONSOLE_ENABLED=False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
-#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-#   'Accept-Language': 'en',
-#}
+DEFAULT_REQUEST_HEADERS = {
+  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+  'Accept-Language': 'en',
+  'Referer':'http://jandan.net/ooxx'
+}
+
 
 # Enable or disable spider middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
